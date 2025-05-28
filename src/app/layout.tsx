a4a523ac874c555a854630a81444d03751d5b8dc
jomon <jomon.h@gmail.com>
2025-05-29 07:03:05 +0800

    20250529

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,27 @@ export const metadata: Metadata = {
   title: "QQQ Investment Calculator 📈💰",
   description: "Welcome to the QQQ Investment Calculator, a web application designed to help you simulate and visualize potential returns on investments in QQQ (Invesco QQQ Trust, an exchange-traded fund based on the Nasdaq-100 Index). This tool allows you to input various investment parameters and see the projected growth over time.",
   keywords: ["QQQ", "Investment", "Calculator", "Nasdaq-100", "ETF", "Stock Market", "Finance", "Returns", "Investment Simulator", "Investment Visualization", "Financial Planning", "Invesco QQQ Trust"],
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-36x36.png', sizes: '36x36', type: 'image/png' },
+      { url: '/favicon-48x48.png', sizes: '48x48', type: 'image/png' },
+      { url: '/favicon-57x57.png', sizes: '57x57', type: 'image/png' },
+      { url: '/favicon-60x60.png', sizes: '60x60', type: 'image/png' },
+      { url: '/favicon-72x72.png', sizes: '72x72', type: 'image/png' },
+      { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
+      { url: '/favicon-114x114.png', sizes: '114x114', type: 'image/png' },
+      { url: '/favicon-120x120.png', sizes: '120x120', type: 'image/png' },
+      { url: '/favicon-144x144.png', sizes: '144x144', type: 'image/png' },
+      { url: '/favicon-152x152.png', sizes: '152x152', type: 'image/png' },
+      { url: '/favicon-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/favicon-512x512.png', sizes: '512x512', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+  },
 };
 
 export default function RootLayout({
